7333d72fdb3fadf22eb809bdd8be6572bff7a04d
Ben A. Morgan <ben@benmorgan.io>
2017-02-03 15:58:11 -0500

    turn reconfirmable off in devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,6 +2,8 @@ Devise.setup do |config|
   # Required so users don't lose their carts when they need to confirm.
   config.allow_unconfirmed_access_for = 1.days
 
+  config.reconfirmable = false
+
   # Fixes the bug where Confirmation errors result in a broken page.
   config.router_name = :spree
 
